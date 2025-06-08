e5f7c76dd8ab8ad207771088e6a639ff40427247
MatyRom <105615131+MatyRom@users.noreply.github.com>
2025-06-08 17:50:23 -0300

    Automatizaciones y Estado
    Se agrega Consultar automatizaciones y su estado actual

--- gestion_automatizacion.py
+++ gestion_automatizacion.py
@@ -1,5 +1,10 @@
 from gestion_dispositivos import dispositivos
 
+estado_automatizaciones = {
+    'Modo Noche': {'estado': 'No ejecutada'},
+    'Modo Seguridad': {'estado': 'No ejecutada'}
+}
+
 def automatizacion_modo_noche():
     """Automatización: Enciende todas las luces apagadas para el modo noche."""
     print("\n__Automatización: Modo Noche __")
@@ -15,6 +20,7 @@ def automatizacion_modo_noche():
         print("No hay luces apagadas para encender o no hay luces registradas.")
     else:
         print(f"Modo noche activado: {luces_encendidas} luces encendidas.")
+        estado_automatizaciones['Modo Noche']['estado'] = 'Ejecutado'
 
 def automatizacion_modo_seguridad():
     """Automatización: Enciende todas las cámaras y alarmas para el modo seguridad."""
@@ -38,3 +44,10 @@ def automatizacion_modo_seguridad():
         print("No hay cámaras o alarmas apagadas para activar, o no hay dispositivos de seguridad registrados.")
     else:
         print(f"Modo seguridad activado: {camaras_activadas} cámaras y {alarmas_activadas} alarmas activadas.")
+        estado_automatizaciones['Modo Seguridad']['estado'] = 'Ejecutado'
+
+def listar_automatizaciones():
+        print("\n__ Automatizaciones Disponibles __")
+        for nombre, automatizaciones in estado_automatizaciones.items():
+            print(f"- {nombre}:")
+            print(f"  Estado: {automatizaciones['estado']}")
